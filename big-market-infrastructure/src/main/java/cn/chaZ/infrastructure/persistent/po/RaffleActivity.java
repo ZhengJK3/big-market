a52ed066ec6aaccce0f2d944f9a97cc842fef4b9
山己 <13199865+zhengjk03@user.noreply.gitee.com>
2024-05-24 16:57:18 +0800

    feat: 第一阶段开始，用户参与抽奖活动表设计

---
+++ big-market-infrastructure/src/main/java/cn/chaZ/infrastructure/persistent/po/RaffleActivity.java
@@ -0,0 +1,54 @@
+package cn.chaZ.infrastructure.persistent.po;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @program: big-market
+ * @description: 抽奖活动表 持久化对象
+ * @author: chaZ
+ * @create: 2024-05-24 16:49
+ **/
+@Data
+public class RaffleActivity {
+    /** 自增ID */
+    private Long id;
+
+    /** 活动ID */
+    private Long activityId;
+
+    /** 活动名称 */
+    private String activityName;
+
+    /** 活动描述 */
+    private String activityDesc;
+
+    /** 开始时间 */
+    private Date beginDateTime;
+
+    /** 结束时间 */
+    private Date endDateTime;
+
+    /** 库存总量 */
+    private Integer stockCount;
+
+    /** 剩余库存 */
+    private Integer stockCountSurplus;
+
+    /** 活动参与次数配置 */
+    private Long activityCountId;
+
+    /** 抽奖策略ID */
+    private Long strategyId;
+
+    /** 活动状态 */
+    private String state;
+
+    /** 创建时间 */
+    private Date createTime;
+
+    /** 更新时间 */
+    private Date updateTime;
+
+}
